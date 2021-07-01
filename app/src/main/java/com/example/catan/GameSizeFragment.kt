d4b79891caca4ec15fe339a01e41c5d6ed62c0e2
Fernando Barra <fernandobarra2207@gmail.com>
2021-07-01 16:14:32 -0600

    Create host fragment
    Creates a GameSizeFragment that prompts the user to select
    the size of the map. It doesn't do anything with the input.
    This fragment is the host fragment.

--- app/src/main/java/com/example/catan/GameSizeFragment.kt
+++ app/src/main/java/com/example/catan/GameSizeFragment.kt
@@ -1,6 +1,18 @@
 package com.example.catan
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class GameSizeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_game_size, container, false)
+    }
 }
